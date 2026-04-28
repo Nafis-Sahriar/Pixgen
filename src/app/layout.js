5974271499a6cd528492a14f316d12da6e9e34b7
Nafis-Sahriar <nafis.apid@gmail.com>
2026-04-28 20:03:22 +0600

    First Commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,18 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Outfit } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/Components/Navbar";
+import Footer from "@/Components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
+
+const outfit = Outfit({
+
+  subsets: ["latin"]
+
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -20,9 +28,15 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      data-theme="light"
+      className={`${outfit.className} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar></Navbar>
+        {children}
+        <Footer></Footer>
+        
+        </body>
     </html>
   );
 }
